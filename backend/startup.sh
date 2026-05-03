51d9febdce8c4a79556b73a45c9922576d8b4c5c
Suhani Sharma <suhanisharma150708@gmail.com>
2026-05-03 10:19:16 +0530

    Update startup.sh

--- backend/startup.sh
+++ backend/startup.sh
@@ -6,4 +6,4 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 # 3. Start the server
-gunicorn config.wsgi:application --bind 0.0.0.0:8000
+gunicorn config.wsgi:application --bind 0.0.0.0:8000 --timeout 120
