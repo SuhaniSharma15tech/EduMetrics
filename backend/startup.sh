a5a2fd7978e1915e0f689e8dd1cd8e1a91c6c6a0
Suhani Sharma <suhanisharma150708@gmail.com>
2026-05-03 09:11:01 +0530

    Update startup.sh

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+python manage.py migrate analysis_engine 0002 --fake --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 gunicorn config.wsgi:application --bind 0.0.0.0:8000
